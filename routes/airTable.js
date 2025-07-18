889e71a7db1829cea9c78963cd636dd59460a8e0
Hafiz Ali <hafizali@bloomscape.com>
2025-07-18 17:55:34 +0500

    ...

--- routes/airTable.js
+++ routes/airTable.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const AirTableController = require("../controllers/AirTableController");
+const AirTableController = require("../controllers/airTableController");
 
 router.get("/:tableName", AirTableController.getAllData);
 router.get("/orders/sync", AirTableController.syncOverAllAirTableWithDb);  
